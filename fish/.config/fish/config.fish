082e139ab4d1bd49936a2e5fcd839f45b106ea1e
Jonny Irwin <git@jbi.im>
2022-11-05 21:58:10 +0000

    remove greeting message

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,17 +1,9 @@
-#set theme_color_scheme nord
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+set -u fish_greeting
+set STARSHIP_CONFIG $HOME/.config/starship.toml
+starship init fish | source
+
 set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
-set -gx PATH "$HOME/.local/bin" $PATH
-set -gx FZF_DEFAULT_COMMAND "rg --files --hidden --glob '!.git' --glob '!node_modules'"
-
-abbr di 'cd ~/git/DigitalInnovation'
-
-set -gx DOCKER_GROUP_ID (getent group docker | cut -d: -f3)
-
-set theme_color_scheme terminal2-dark
-
-# Base16 Shell
-if status --is-interactive
-    set BASE16_SHELL "$HOME/.config/base16-shell/"
-    source "$BASE16_SHELL/profile_helper.fish"
-end
