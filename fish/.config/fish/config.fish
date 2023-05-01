8ce39c38f6c9725b24f3c29ed47348a7dee3c955
Jonny Irwin <git@jbi.im>
2023-05-01 11:45:33 +0100

    adding volta to fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,6 +5,6 @@ set -u fish_greeting
 set STARSHIP_CONFIG $HOME/.config/starship.toml
 starship init fish | source
 
-set -gx VOLTA_HOME "$HOME/.volta"
 set -gx VAGRANT_DEFAULT_PROVIDER libvirt
-set -gx PATH "$HOME/.local/bin $VOLTA_HOME/bin" $PATH
+set -gx VOLTA_HOME "$HOME/.volta"
+set -gx PATH "$VOLTA_HOME/bin" $PATH
