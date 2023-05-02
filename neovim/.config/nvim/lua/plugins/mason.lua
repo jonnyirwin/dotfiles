fc474701770e688fd54b6e6c9151ce163279533d
Jonny Irwin <git@jbi.im>
2023-05-02 20:48:07 +0100

    added nvim tree and basic mason setup:wq

---
+++ neovim/.config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,7 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		run = ":MasonUpdate",
+		config = true
+	}
+}
---
+++ neovim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,10 @@
+return {
+	{
+		"nvim-tree/nvim-tree.lua",
+		lazy = true,
+		config = true,
+		keys = {
+			{ "<leader>e", ":NvimTreeToggle<cr>", desc = "NvimTree" }			
+		}
+	}
+}
