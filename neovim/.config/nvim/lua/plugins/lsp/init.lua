4d72c2827cfa4d5d33e587b6053dbc40ae230519
Jonny Irwin <git@jbi.im>
2023-05-04 11:03:57 +0100

    added neodev

--- neovim/.config/nvim/lua/plugins/lsp/init.lua
+++ neovim/.config/nvim/lua/plugins/lsp/init.lua
@@ -4,15 +4,28 @@ return {
 		dependencies = {
 			"williamboman/mason.nvim",
 			"williamboman/mason-lspconfig.nvim",
-			"hrsh7th/cmp-nvim-lsp"
+			"hrsh7th/cmp-nvim-lsp",
+			"folke/neodev.nvim"
 		},
 		run = ":MasonUpdate",
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 			require("mason").setup()
 			require("mason-lspconfig").setup()
-			require("lspconfig").tsserver.setup({
+			require("neodev").setup()
+			local lspconfig = require("lspconfig")
+			lspconfig.tsserver.setup({
 				capabilities = capabilities
+			})
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+				settings = {
+					Lua = {
+						completion = {
+							callSnippet = "Replace"
+						}
+					}
+				}
 			})
 		end
 	}
