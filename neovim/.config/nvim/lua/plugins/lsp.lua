2b7a90b88de9d741b07ed5b86ac195356217eff2
Jonny Irwin <git@jbi.im>
2025-04-14 15:19:54 +0100

    updates

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -1,100 +1,100 @@
--- LSP configuration
+  -- LSP configuration
 
-return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup({
-        ui = {
-          icons = {
-            server_installed = "✓",
-            server_pending = "➜",
-            server_uninstalled = "✗"
+  return {
+    {
+      "williamboman/mason.nvim",
+      config = function()
+        require("mason").setup({
+          ui = {
+            icons = {
+              server_installed = "✓",
+              server_pending = "➜",
+              server_uninstalled = "✗"
+            }
           }
-        }
-      })
-    end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        -- List of servers to automatically install
-        ensure_installed = {
-          "lua_ls",
-          "tsserver",
-          "csharp_ls",
-          "ruby_ls",     -- Changed from solargraph to ruby_ls
-          "pyright",
-          "html",
-          "cssls",
-          "jsonls",
-        },
-        automatic_installation = true,
-      })
-    end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      "blink-cmp/cmp-lsp",
-      "williamboman/mason-lspconfig.nvim"
+        })
+      end
     },
-    config = function()
-      -- LSP settings
-      local lspconfig = require("lspconfig")
-      
-      -- Define on_attach function
-      local on_attach = function(client, bufnr)
-        -- Enable completion triggered by <c-x><c-o>
-        vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-      
-        -- Buffer local mappings
-        local opts = { noremap=true, silent=true, buffer=bufnr }
+    {
+      "williamboman/mason-lspconfig.nvim",
+      config = function()
+        require("mason-lspconfig").setup({
+          -- List of servers to automatically install
+          ensure_installed = {
+            "lua_ls",
+            "ts_ls",
+            "csharp_ls",
+            "ruby_ls",     -- Changed from solargraph to ruby_ls
+            "pyright",
+            "html",
+            "cssls",
+            "jsonls",
+          },
+          automatic_installation = true,
+        })
+      end
+    },
+    {
+      "neovim/nvim-lspconfig",
+      dependencies = {
+        "saghen/blink.cmp",
+        "williamboman/mason-lspconfig.nvim"
+      },
+      config = function()
+        -- LSP settings
+        local lspconfig = require("lspconfig")
         
-        -- LSP keybindings
-        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-        vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-        vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
-        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-        vim.keymap.set('n', '<leader>cf', function() vim.lsp.buf.format { async = true } end, opts) -- Fixed: was <leader>f
-      end
-      
-      -- Setup capabilities with blink-cmp
-      local capabilities
-      if pcall(require, 'blink-cmp.lsp') then
-        capabilities = require('blink-cmp.lsp').default_capabilities()
-      else
-        capabilities = vim.lsp.protocol.make_client_capabilities()
-      end
-      
-      -- Configure Lua server for Neovim development
-      lspconfig.lua_ls.setup {
-        on_attach = on_attach,
-        capabilities = capabilities,
-        settings = {
-          Lua = {
-            diagnostics = {
-              globals = { 'vim' }
-            },
-            workspace = {
-              library = vim.api.nvim_get_runtime_file("", true),
-              checkThirdParty = false
-            },
-            telemetry = {
-              enable = false,
+        -- Define on_attach function
+        local on_attach = function(client, bufnr)
+          -- Enable completion triggered by <c-x><c-o>
+          vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+        
+          -- Buffer local mappings
+          local opts = { noremap=true, silent=true, buffer=bufnr }
+          
+          -- LSP keybindings
+          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+          vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+          vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+          vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+          vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+          vim.keymap.set('n', '<leader>cf', function() vim.lsp.buf.format { async = true } end, opts) -- Fixed: was <leader>f
+        end
+        
+        -- Setup capabilities with blink-cmp
+        local capabilities
+        if pcall(require, 'blink-cmp.lsp') then
+          capabilities = require('blink-cmp.lsp').default_capabilities()
+        else
+          capabilities = vim.lsp.protocol.make_client_capabilities()
+        end
+        
+        -- Configure Lua server for Neovim development
+        lspconfig.lua_ls.setup {
+          on_attach = on_attach,
+          capabilities = capabilities,
+          settings = {
+            Lua = {
+              diagnostics = {
+                globals = { 'vim' }
+              },
+              workspace = {
+                library = vim.api.nvim_get_runtime_file("", true),
+                checkThirdParty = false
+              },
+              telemetry = {
+                enable = false,
+              },
             },
           },
-        },
-      }
-      
-      -- Setup other LSP servers
-      local servers = { 
-        "tsserver", 
+        }
+        
+        -- Setup other LSP servers
+        local servers = { 
+        "ts_ls", 
         "csharp_ls", 
         "ruby_ls",     -- Changed from solargraph to ruby_ls
         "pyright", 
