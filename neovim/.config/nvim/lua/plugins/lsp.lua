bb546f11aa70eb6a13e33953963924335e0a586a
Jonny Irwin <git@jbi.im>
2025-04-27 16:03:17 +0100

    updated keybindings

--- neovim/.config/nvim/lua/plugins/lsp.lua
+++ neovim/.config/nvim/lua/plugins/lsp.lua
@@ -48,16 +48,16 @@ return {
 				lspconfig.ruby_lsp.setup({})
 				lspconfig.ts_ls.setup({})
 
-				vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, { desc = "Declaration" })
-				vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "Definition" })
-				vim.keymap.set("n", "<leader>gh", vim.lsp.buf.hover, { desc = "Hover" })
-				vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, { desc = "Implementation" })
-				vim.keymap.set("n", "<leader>gs", vim.lsp.buf.signature_help, { desc = "Signature Help" })
-				vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, { desc = "References" })
-				vim.keymap.set("n", "<leader>gc", vim.lsp.buf.rename, { desc = "Rename" })
-				vim.keymap.set("n", "<leader>ga", vim.lsp.buf.code_action, { desc = "Code Action" })
-				vim.keymap.set("n", "<leader>gj", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
-				vim.keymap.set("n", "<leader>gk", vim.diagnostic.goto_prev, { desc = "Prev diagnostic" })
+				vim.keymap.set("n", "<leader>lD", vim.lsp.buf.declaration, { desc = "Declaration" })
+				vim.keymap.set("n", "<leader>ld", vim.lsp.buf.definition, { desc = "Definition" })
+				vim.keymap.set("n", "<leader>lh", vim.lsp.buf.hover, { desc = "Hover" })
+				vim.keymap.set("n", "<leader>li", vim.lsp.buf.implementation, { desc = "Implementation" })
+				vim.keymap.set("n", "<leader>ls", vim.lsp.buf.signature_help, { desc = "Signature Help" })
+				vim.keymap.set("n", "<leader>lr", vim.lsp.buf.references, { desc = "References" })
+				vim.keymap.set("n", "<leader>lc", vim.lsp.buf.rename, { desc = "Rename" })
+				vim.keymap.set("n", "<leader>la", vim.lsp.buf.code_action, { desc = "Code Action" })
+				vim.keymap.set("n", "<leader>lj", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
+				vim.keymap.set("n", "<leader>lk", vim.diagnostic.goto_prev, { desc = "Prev diagnostic" })
 			end
 		},
 }
--- neovim/.config/nvim/lua/plugins/which-key.lua
+++ neovim/.config/nvim/lua/plugins/which-key.lua
@@ -2,6 +2,46 @@ return {
     "folke/which-key.nvim",
     event = "VeryLazy",
     opts = {
-
-    }
+        icons = {
+            breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
+            separator = "➜", -- symbol used between a key and its label
+            group = "+", -- symbol prepended to a group
+        },
+        window = {
+            border = "rounded", -- none, single, double, shadow
+        },
+    },
+    config = function(_, opts)
+        local wk = require("which-key")
+        wk.setup(opts)
+        
+        -- Register group names based on actual configured keybindings
+        wk.register({
+            ["<leader>f"] = { name = "+Find (Telescope)" },  -- Configured in telescope.lua
+            ["<leader>l"] = { name = "+LSP" },               -- Updated to match lsp.lua
+            ["<leader>c"] = { name = "+Copilot" },           -- Configured in copilot.lua
+            ["<leader>b"] = { name = "+Buffer" },            -- Active in keymap.lua
+            ["<leader>w"] = { name = "+Window" },            -- For navigation
+            ["<leader>u"] = { name = "+UI Toggles" },        -- Active for <leader>uh in keymap.lua
+            ["<leader>x"] = { name = "+Trouble" },           -- Configured in trouble.lua
+            ["<leader>d"] = { name = "+Debug/Diagnostics" }, -- From keymap.lua and possibly nvim-dap
+            
+            -- Individual keys
+            ["<leader>e"] = { "<CMD>Oil --float<CR>", "Open Oil Explorer" }, -- From oil.lua
+            ["<leader>gf"] = { function() vim.lsp.buf.format({ async = true }) end, "Format buffer" }, -- From keymap.lua
+        })
+        
+        -- Add additional groups when you implement their keybindings
+        -- Uncomment as you implement them:
+        --
+        -- ["<leader>g"] = { name = "+Git" },
+        -- ["<leader>r"] = { name = "+Rails/Ruby" },
+        -- ["<leader>t"] = { name = "+Test" },
+        -- ["<leader>a"] = { name = "+Tabs" },
+        -- ["<leader>i"] = { name = "+Swap/Sort" },
+        -- ["<leader>h"] = { name = "+Hunks (Git)" },
+        -- ["<leader>p"] = { name = "+Project/Files" },
+        -- ["<leader>v"] = { name = "+Vimux" },
+        -- ["<leader>m"] = { name = "+Manage/Motion" },
+    end
 }
