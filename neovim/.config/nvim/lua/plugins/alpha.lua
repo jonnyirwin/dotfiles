c013c6750e929836a478fae64ff6f4a0736acd75
Jonny Irwin <git@jbi.im>
2025-08-29 23:32:09 +0100

    dank mono font

--- neovim/.config/nvim/lua/plugins/alpha.lua
+++ neovim/.config/nvim/lua/plugins/alpha.lua
@@ -60,9 +60,9 @@ return {
 			dashboard.button("f", "📁 Find file", ":Telescope find_files <CR>"),
 			dashboard.button("r", "📄 Recent files", ":Telescope oldfiles <CR>"),
 			dashboard.button("t", "🔍 Find text", ":Telescope live_grep <CR>"),
-			dashboard.button("c", "⚙️  Config", ":e ~/.config/nvim/init.lua <CR>"),
+			dashboard.button("c", "⚙️ Config", ":e ~/.config/nvim/init.lua <CR>"),
 			dashboard.button("o", "📂 File Manager", ":Oil <CR>"),
-			dashboard.button("u", "⬇️  Update", ":Lazy<CR>"),
+			dashboard.button("u", "⬇️ Update", ":Lazy<CR>"),
 			dashboard.button("q", "❌ Quit", ":qa<CR>"),
 		}			for _, button in ipairs(dashboard.section.buttons.val) do
 				button.opts.hl = "AlphaButtons"
